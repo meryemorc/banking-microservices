25c5e3b6483648c84947a8b101faf55b37eb7436
meryem <116671202+meryemorc@users.noreply.github.com>
2025-11-22 23:26:00 +0300

    transaction

--- services/transactionService/src/main/java/com/example/transactionservice/repository/TransactionRepository.java
+++ services/transactionService/src/main/java/com/example/transactionservice/repository/TransactionRepository.java
@@ -1,13 +1,14 @@
 package com.example.transactionservice.repository;
 
 import com.example.transactionservice.model.TransactionModel;
-import jakarta.transaction.Transaction;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface TransactionRepository extends JpaRepository<TransactionModel,Long> {
 
     List<TransactionModel> findByUserId(Long userId);
