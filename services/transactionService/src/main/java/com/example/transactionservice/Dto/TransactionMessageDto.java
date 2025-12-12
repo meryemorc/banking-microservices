14d5ea7ce20b68404e03a8198d70b88a32b58321
meryem <116671202+meryemorc@users.noreply.github.com>
2025-12-12 14:39:12 +0300

    rabbitmq entegrasyonu tamamlandı

---
+++ services/transactionService/src/main/java/com/example/transactionservice/Dto/TransactionMessageDto.java
@@ -0,0 +1,22 @@
+package com.example.transactionservice.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TransactionMessageDto implements Serializable {
+
+    private Long userId;
+    private String sourceAccountNumber;
+    private String targetAccountNumber;
+    private BigDecimal amount;
+    private String transactionType;
+    private String transactionStatus;
+    private String description;
+}
